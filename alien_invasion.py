a8ce3aebed5a1cd1701d100a8eb66a0e43c7a52a
Gavin <491038876@qq.com>
2017-05-26 13:38:38 +0800

    second commit -- add Ship class and screen ship in the bottom

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 
 from ai_settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -10,7 +11,9 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-
+	
+	#创建一艘飞船
+	ship = Ship(screen)
 	#开始游戏的主循环
 	while  True:
 		#监视键盘和鼠标事件
@@ -19,6 +22,8 @@ def run_game():
 				sys.exit()
 		#每次循环时都重绘屏幕
 		screen.fill(ai_settings.bg_color)
+		ship.blitme()
+
 		#让新绘制的屏幕可见
 		pygame.display.flip()
 
---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+#创建ship类
+class Ship():
+	"""初始化飞船"""
+	def __init__(self, screen):
+		self.screen = screen
+		#加载飞船图像并获取外接矩形
+		self.image = pygame.image.load('images/ship.bmp')
+		self.rect = self.image.get_rect()
+		self.screen_rect = screen.get_rect()
+
+		#将每艘新飞船放到底部
+		self.rect.centerx = self.screen_rect.centerx
+		self.rect.bottom = self.screen_rect.bottom
+
+	def blitme(self):
+		#指定位置绘制飞船
+		self.screen.blit(self.image, self.rect)
